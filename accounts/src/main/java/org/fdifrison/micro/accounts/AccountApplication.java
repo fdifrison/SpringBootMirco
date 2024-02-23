81929fe17cae000ba58da18b7edfd2a0587d28d9
giovanni.frison <giovanni.frison@alten.it>
2024-02-23 19:02:08 +0100

    feat: adding swagger & openapi
    Signed-off-by: giovanni.frison <giovanni.frison@alten.it>

--- accounts/src/main/java/org/fdifrison/micro/accounts/AccountApplication.java
+++ accounts/src/main/java/org/fdifrison/micro/accounts/AccountApplication.java
@@ -1,11 +1,14 @@
 package org.fdifrison.micro.accounts;
 
+//import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition
 public class AccountApplication {
 
     public static void main(String[] args) {
