fd90d6e42740a4af493f4645315819618037ba92
giovanni.frison <giovanni.frison@alten.it>
2024-02-29 17:15:28 +0100

    update: enabled actuator refresh endpoint
    Signed-off-by: giovanni.frison <giovanni.frison@alten.it>

--- accounts/src/main/java/org/fdifrison/micro/accounts/config/AppContactInfo.java
+++ accounts/src/main/java/org/fdifrison/micro/accounts/config/AppContactInfo.java
@@ -1,10 +1,19 @@
 package org.fdifrison.micro.accounts.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AppContactInfo(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public final class AppContactInfo {
+    private  String message;
+    private  Map<String, String> contactDetails;
+    private  List<String> onCallSupport;
+
 }
--- cards/src/main/java/org/fdifrison/micro/cards/config/AppContactInfo.java
+++ cards/src/main/java/org/fdifrison/micro/cards/config/AppContactInfo.java
@@ -1,10 +1,18 @@
 package org.fdifrison.micro.cards.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AppContactInfo(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public final class AppContactInfo {
+    private  String message;
+    private  Map<String, String> contactDetails;
+    private  List<String> onCallSupport;
+
 }
