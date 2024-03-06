42fd418ca962380f9c602ffb7c9d2ab4b1805b31
giovanni.frison <giovanni.frison@alten.it>
2024-03-06 14:58:04 +0100

    update<gateway>: adding standard filter to routing
    Signed-off-by: giovanni.frison <giovanni.frison@alten.it>

--- gateway-server/src/main/java/org/fdifrison/micro/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/org/fdifrison/micro/gatewayserver/GatewayServerApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class GatewayServerApplication {
 
@@ -18,13 +20,16 @@ public class GatewayServerApplication {
     public RouteLocator fdifrisonRouteConfig(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(path -> path.path("/fdifrison/accounts/**")
-                        .filters(f -> f.rewritePath("/fdifrison/accounts/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f.rewritePath("/fdifrison/accounts/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://ACCOUNTS")) // ms name all CAPS as defined inside the eureka server
                 .route(path -> path.path("/fdifrison/loans/**")
-                        .filters(f -> f.rewritePath("/fdifrison/loans/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f.rewritePath("/fdifrison/loans/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://LOANS"))
                 .route(path -> path.path("/fdifrison/cards/**")
-                        .filters(f -> f.rewritePath("/fdifrison/cards/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f.rewritePath("/fdifrison/cards/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://CARDS"))
                 .build();
     }
