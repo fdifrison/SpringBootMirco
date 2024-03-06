0e4b605d79595c68d21c2fc178e2cd8b1e3ab24d
giovanni.frison <giovanni.frison@alten.it>
2024-03-06 16:58:52 +0100

    update<all>: adding scripts for docker image generation and pushing to docker.io
    Signed-off-by: giovanni.frison <giovanni.frison@alten.it>

---
+++ dockerImageCreator.sh
@@ -0,0 +1,47 @@
+cd accounts || exit
+
+mvn clean install -DskipTests
+
+mvn compile jib:dockerBuild -DskipTests
+
+cd ..
+
+cd cards || exit
+
+mvn clean install -DskipTests
+
+mvn compile jib:dockerBuild -DskipTests
+
+cd ..
+
+cd loans || exit
+
+mvn clean install -DskipTests
+
+mvn compile jib:dockerBuild -DskipTests
+
+cd ..
+
+cd config-server || exit
+
+mvn clean install -DskipTests
+
+mvn compile jib:dockerBuild -DskipTests
+
+cd ..
+
+cd gateway-server || exit
+
+mvn clean install -DskipTests
+
+mvn compile jib:dockerBuild -DskipTests
+
+cd ..
+
+cd service-discovery-agent || exit
+
+mvn clean install -DskipTests
+
+mvn compile jib:dockerBuild -DskipTests
+
+cd ..
