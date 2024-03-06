a51f84fe5167a3ae279a453aa5e187518414d6a7
giovanni.frison <giovanni.frison@alten.it>
2024-03-06 14:54:03 +0100

    update<gateway>: adding routing custom specification to api gateway
    Signed-off-by: giovanni.frison <giovanni.frison@alten.it>

--- gateway-server/src/main/java/org/fdifrison/micro/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/org/fdifrison/micro/gatewayserver/GatewayServerApplication.java
@@ -2,12 +2,31 @@ package org.fdifrison.micro.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServerApplication.class, args);
+    }
+
+    // Create a custom redirect policy to allow for "fdifrison" in front of ms names
+    @Bean
+    public RouteLocator fdifrisonRouteConfig(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(path -> path.path("/fdifrison/accounts/**")
+                        .filters(f -> f.rewritePath("/fdifrison/accounts/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://ACCOUNTS")) // ms name all CAPS as defined inside the eureka server
+                .route(path -> path.path("/fdifrison/loans/**")
+                        .filters(f -> f.rewritePath("/fdifrison/loans/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://LOANS"))
+                .route(path -> path.path("/fdifrison/cards/**")
+                        .filters(f -> f.rewritePath("/fdifrison/cards/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://CARDS"))
+                .build();
     }
 
 }
