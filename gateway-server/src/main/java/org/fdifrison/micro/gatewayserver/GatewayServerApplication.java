21016be2f2404ca6df5cefc723e906e783370f50
giovanni.frison <giovanni.frison@alten.it>
2024-03-07 17:48:38 +0100

    update<Res4j>: adding the rate limiter pattern to limit incoming requests using Redis RateLimiter
    Signed-off-by: giovanni.frison <giovanni.frison@alten.it>

--- gateway-server/src/main/java/org/fdifrison/micro/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/org/fdifrison/micro/gatewayserver/GatewayServerApplication.java
@@ -2,10 +2,13 @@ package org.fdifrison.micro.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -45,9 +48,23 @@ public class GatewayServerApplication {
                         .uri("lb://LOANS"))
                 .route(path -> path.path("/fdifrison/cards/**")
                         .filters(f -> f.rewritePath("/fdifrison/cards/(?<segment>.*)", "/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+                                        .setKeyResolver(userKeyResolver())))
                         .uri("lb://CARDS"))
                 .build();
+    }
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter() {
+        return  new RedisRateLimiter(1,1,1);
+    }
+
+    @Bean
+    // logic upon which we apply the rate limiter
+    KeyResolver userKeyResolver() {
+        return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("user"))
+                .defaultIfEmpty("anonymous");
     }
 
 }
