ebc2753b1d8e18a552bbe24be7f07a430f8367b0
giovanni.frison <giovanni.frison@alten.it>
2024-03-07 16:26:25 +0100

    chores<Res4j>: implementing retry patter, setting retries and backoff logic
    Signed-off-by: giovanni.frison <giovanni.frison@alten.it>

--- gateway-server/src/main/java/org/fdifrison/micro/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/org/fdifrison/micro/gatewayserver/GatewayServerApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 import static org.springframework.cloud.gateway.support.RouteMetadataUtils.CONNECT_TIMEOUT_ATTR;
@@ -33,7 +35,13 @@ public class GatewayServerApplication {
                         .uri("lb://ACCOUNTS")) // ms name all CAPS as defined inside the eureka server
                 .route(path -> path.path("/fdifrison/loans/**")
                         .filters(f -> f.rewritePath("/fdifrison/loans/(?<segment>.*)", "/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                // we need to assure that the methods that we give the retry capabilities are actually idempotent
+                                // here we are setting it only on GET methods
+                                .retry(retryConfig -> retryConfig.setRetries(3)
+                                        .setMethods(HttpMethod.GET)
+                                        .setBackoff(Duration.ofMillis(100), Duration.ofSeconds(1), 2, true)))
+                                        // the backoff value is the delay we give at each retry operation
                         .uri("lb://LOANS"))
                 .route(path -> path.path("/fdifrison/cards/**")
                         .filters(f -> f.rewritePath("/fdifrison/cards/(?<segment>.*)", "/${segment}")
