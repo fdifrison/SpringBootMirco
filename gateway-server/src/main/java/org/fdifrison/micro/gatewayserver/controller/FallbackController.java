e0b154f20518b639a09ff3404d1d2baa2a13a400
giovanni.frison <giovanni.frison@alten.it>
2024-03-07 11:54:05 +0100

    update<Res4j>: implementing fallback mechanism instead of runtime exception
    Signed-off-by: giovanni.frison <giovanni.frison@alten.it>

---
+++ gateway-server/src/main/java/org/fdifrison/micro/gatewayserver/controller/FallbackController.java
@@ -0,0 +1,16 @@
+package org.fdifrison.micro.gatewayserver.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+
+    @RequestMapping("/contactSupport")
+    public Mono<String> contactSupport() {
+        return Mono.just("An error occurred. Please try again later or contact the support at wewillhelpyou@maybe.com");
+    }
+
+
+}
