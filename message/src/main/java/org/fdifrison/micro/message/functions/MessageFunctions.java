404f5e74f902c7df458b2d453e75bc4a38695f3e
giovanni.frison <giovanni.frison@alten.it>
2024-03-20 15:48:50 +0100

    update<rabbitMQ>: creating functions for emails and sms Signed-off-by: giovanni.frison <giovanni.frison@alten.it>
    Signed-off-by: giovanni.frison <giovanni.frison@alten.it>

--- message/src/main/java/org/fdifrison/micro/message/functions/MessageFunctions.java
+++ message/src/main/java/org/fdifrison/micro/message/functions/MessageFunctions.java
@@ -1,9 +1,32 @@
 package org.fdifrison.micro.message.functions;
 
+import org.fdifrison.micro.message.dto.AccountsMsgDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
 
 @Configuration
 public class MessageFunctions {
 
+    private final static Logger logger = LoggerFactory.getLogger(MessageFunctions.class);
+
+    @Bean
+    public Function<AccountsMsgDTO, AccountsMsgDTO> email() {
+        return accountsMsgDTO -> {
+            logger.info("Sending email: " + accountsMsgDTO.toString());
+            return accountsMsgDTO;
+        };
+    }
+
+    @Bean
+    public Function<AccountsMsgDTO, Long> sms() {
+        return accountsMsgDTO -> {
+            logger.info("Sending sms: " + accountsMsgDTO.toString());
+            return accountsMsgDTO.accountNumber();
+        };
+    }
 
 }
