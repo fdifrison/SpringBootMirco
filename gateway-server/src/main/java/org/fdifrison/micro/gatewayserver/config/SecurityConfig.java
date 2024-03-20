2396dc4801044a1a9397c48bc1bee7dbc9114f9a
giovanni.frison <giovanni.frison@alten.it>
2024-03-20 11:35:59 +0100

    update<oauth2>: adding authorization ROLES inside keycloak and the KeycloakRoleConverter to handle their parsing and insertion inside securityFilterChain Signed-off-by: giovanni.frison <giovanni.frison@alten.it>
    Signed-off-by: giovanni.frison <giovanni.frison@alten.it>

--- gateway-server/src/main/java/org/fdifrison/micro/gatewayserver/config/SecurityConfig.java
+++ gateway-server/src/main/java/org/fdifrison/micro/gatewayserver/config/SecurityConfig.java
@@ -2,11 +2,17 @@ package org.fdifrison.micro.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -19,14 +25,21 @@ public class SecurityConfig {
                         // the permission that comes first can override the ones that following one
                         // exchange -> exchange.anyExchange().authenticated() // authenticate any exchange
                         exchange -> exchange.pathMatchers(HttpMethod.GET).permitAll() // read-only is permitted without authentication
-                                .pathMatchers("/fdifrison/accounts/**").authenticated()
-                                .pathMatchers("/fdifrison/cards/**").authenticated()
-                                .pathMatchers("/fdifrison/loans/**").authenticated())
+                                .pathMatchers("/fdifrison/accounts/**").hasRole("ACCOUNTS")
+                                .pathMatchers("/fdifrison/cards/**").hasRole("CARDS")
+                                .pathMatchers("/fdifrison/loans/**").hasRole("LOANS"))
                 .oauth2ResourceServer(
                         oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
-                                .jwt(Customizer.withDefaults()));
+                                .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
         security.csrf(ServerHttpSecurity.CsrfSpec::disable); // not needed since there is no browser involved in our app
         return security.build();
     }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+    }
+
 
 }
