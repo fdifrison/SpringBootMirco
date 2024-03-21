6844006806b5265f0df00b515c520a2c73f4861f
giovanni.frison <giovanni.frison@alten.it>
2024-03-21 17:04:16 +0100

    update<k8s>: configurations yml files Signed-off-by: giovanni.frison <giovanni.frison@alten.it>
    Signed-off-by: giovanni.frison <giovanni.frison@alten.it>

---
+++ k8s/k8s-runner.sh
@@ -0,0 +1,19 @@
+kubectl delete -n default deployment keycloak-deployment
+kubectl delete -n default deployment configmaps-deployment
+kubectl delete -n default deployment config-server-deployment
+kubectl delete -n default deployment service-discovery-agent-deployment
+kubectl delete -n default deployment accounts-deployment
+kubectl delete -n default deployment loans-deployment
+kubectl delete -n default deployment cards-deployment
+kubectl delete -n default deployment gateway-server-deployment
+
+
+
+kubectl apply -f keycloak.yml
+kubectl apply -f configmaps.yml
+kubectl apply -f configserver.yml
+kubectl apply -f eurekaserver.yml
+kubectl apply -f accounts.yml
+kubectl apply -f loans.yml
+kubectl apply -f cards.yml
+kubectl apply -f gatewayForLast.yml
